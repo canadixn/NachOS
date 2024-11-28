604e68adf32c85de6dd272be9140ea5862b6f19c
canadixn <gx.xbox360@gmail.com>
2024-11-27 19:53:15 -0600

    Brainrot Additions

--- src/main.asm
+++ src/main.asm
@@ -3,14 +3,59 @@
 org 0x7C00 ; BIOS always puts our operating system at address 0x7C00 --> Give our assembler this information. (See line 4: https://github.com/joeangel/linux-0.0.1/blob/master/boot/boot.s)
 bits 16 ; directive; code should be assembled for a 16-bit  mode (real mode, present on all x86 processes, see https://wiki.osdev.org/Real_Mode#:~:text=Real%20Mode%20is%20a%20simplistic,begin%20execution%20in%20Real%20Mode.)
 
+; y'know how C++ has endl like cout << "lol" << ****endl****; ?? Well, why not just use a NASM's define directive to name a symbol ENDL
+; 0x0D is the carriage return and 0x0A is the line feed; both use the power of friendship to make "\n", which is slang for new line in computer lingo
+; this is just for convenience anyhoo
+%define ENDL 0x0D, 0x0A 
 
-main: ; holy skibidi
-    hlt ; halts the processor (from executing)
+; so the program kinda starts here so we want to instruct the little guy to jump to our main label
+start:
+    jmp main
 
-.halt: 
+
+puts:
+    push si ; source index register (used for string operations, memory tasks, etc) pushes onto the stack
+    push ax ; accumulator register (bro does arithmetic operations ☠️☠️) pushes onto the stack
+    push bx ; base register (storing addresses for data access) pushes onto the stack
+
+; some magic to display a string of characters onto the stream
+.loop:
+    lodsb               
+    or al, al ; is al zero? 0x00 = 0x00 ? if it is zero, zero flag is set, which will check if we've reached the null terminator           
+    jz .done ; reached the null terminator? jump to .done label :)
+
+    mov ah, 0x0E  ; another magic code for teletype output, sets up ah register to specify what want to display on the screen    
+    mov bh, 0    
+    int 0x10 ; we were taught in school not to interrupt yet here we are interrupting the video services
+
+    jmp .loop ; and so shall we loop to the next character of our string
+
+.done:
+    pop bx ; pop it! back you go onto the bx register
+    pop ax ; pop it! back you go onto the ax register
+    pop si ; pop it! back you go onto the si register    
+    ret ; oki return
+    
+main:
+    mov ax, 0          
+    mov ds, ax
+    mov es, ax
+    
+    mov ss, ax
+    mov sp, 0x7C00      
+
+    mov si, msg_skibidi
+    call puts
+
+    hlt
+
+.halt:
     jmp .halt
 
-times 510-($-$$) db 0 ; Padding to 510 bytes; reserving two for the boot signature
 
-dw 0AA55h ; Boot signature. dw defines a word (16-bit value). 0xAA55 (magic number), boot signature required for the BIOS to recognize this sector as bootable. THIS SIGNALS TO THE BIOS THAT THIS IS A VALID BOOTLOADER!
 
+msg_skibidi: db 'spit on that thang', ENDL, 0 ; yup, it's all over the screen
+
+
+times 510-($-$$) db 0
+dw 0AA55h
